8a15205946dbb3010e796652de519c4f59fbc09b
alena <bonda5229@gmail.com>
2021-11-08 10:35:28 +0200

    made it possible to filter by url

--- client/src/FilterUsers.js
+++ client/src/FilterUsers.js
@@ -1,27 +1,33 @@
 import {nationalities, gender} from './data/data.js'
+import React  from "react";
+import {useLocation, useHistory} from "react-router-dom";
 
-export default function FilterUsers(props) {
+export default function FilterUsers() {
+    let history = useHistory();
+    const location = useLocation();
+    let query = new URLSearchParams(location.search);
 
     function applyFilters(e) {
         e.preventDefault();
         let form = e.target;
         let formData = new FormData(form)
-        let filterData = {
-            gender: formData.get('gender'),
-            nationality: formData.getAll('nationality'),
-        }
 
-        props.setFilter(filterData);
+        const params = new URLSearchParams({
+            gender: formData.get('gender'),
+            nationality: formData.getAll('nationality')
+        });
+        history.push(location.pathname + "?" + params.toString());
     }
 
+    let nationalityDefaultValues = query.get('nationality') ? query.get('nationality').split(','): [];
     return (
         <form onSubmit={applyFilters}>
-            <select name="gender" className="filterGender">
+            <select name="gender" className="filterGender" defaultValue={query.get('gender')}>
                 {gender.map(item => {
                     return <option value={item} key={item}>{item}</option>
                 })}
             </select>
-            <select name="nationality" className="filterNationality" size="4" multiple>
+            <select name="nationality" className="filterNationality" size="4" multiple defaultValue={nationalityDefaultValues}>
                 <option value="All">All</option>
                 {nationalities .map(item => {
                     return <option value={item} key={item}>{item}</option>
--- client/src/UsersList.js
+++ client/src/UsersList.js
@@ -1,18 +1,22 @@
 import useFetch from "react-fetch-hook";
 import './index.css';
+import {useLocation} from "react-router-dom";
+import React from "react";
 
 const API_URL = 'https://randomuser.me/api';
 const USERS_URL = API_URL + '?results=15&?inc=gender,email,dob,name,picture,nat,id';
 
-export default function UsersList(props) {
-    let filterData = props.filter;
+export default function UsersList() {
+    const location = useLocation();
+    let query = new URLSearchParams(location.search);
+
     let filterUrl = USERS_URL;
 
-    if (filterData.nationality && filterData.nationality.length && filterData.nationality !== 'All') {
-        filterUrl = filterUrl + '&nat=' + filterData.nationality.join(',');
+    if (query.get('nationality') && query.get('nationality') !== 'All') {
+        filterUrl = filterUrl + '&nat=' + query.get('nationality');
     }
-    if (filterData.gender) {
-        filterUrl = filterUrl + '&gender=' + filterData.gender;
+    if (query.get('gender')) {
+        filterUrl = filterUrl + '&gender=' + query.get('gender');
     }
     const {isLoading, error, data} = useFetch(filterUrl);
     if (isLoading) {
@@ -27,7 +31,7 @@ export default function UsersList(props) {
             const dateUsers = new Date(user.dob.date).toISOString().split('T')[0];
             return (
                 <div className="wrapper_user" key={user.email}>
-                    <img src={user.picture.large}/>
+                    <img src={user.picture.large} alt="picture"/>
                     <p className="name">{user.name.first} {user.name.last}</p>
                     <p>{user.gender}</p>
                     <p>{user.email}</p>
